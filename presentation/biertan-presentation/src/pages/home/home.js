036b8e313ab6855215b8cfe6f75f3d04495e5e76
Dan Andreiu <andreiu_dan@yahoo.com>
2025-03-28 23:22:32 +0200

    added alternative text for img

--- presentation/biertan-presentation/src/pages/home/home.js
+++ presentation/biertan-presentation/src/pages/home/home.js
@@ -3,7 +3,7 @@ import './home.css';
 function Home() {
     return (
         <div className="home">
-            <img src="images/Biertan.jpg"></img>
+            <img src="images/Biertan.jpg" alt='background'></img>
 
             <div className="shortcuts">
                 <div className="shortcut-button">
