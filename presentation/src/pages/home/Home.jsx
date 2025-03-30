d21a0b0c699575fa29084e2d95fddd45c0bd34d8
Dan Andreiu <andreiu_dan@yahoo.com>
2025-03-30 19:17:13 +0300

    bug fix
    test failing on ubuntu, not fining the css file

--- presentation/src/pages/home/Home.jsx
+++ presentation/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-import "./Home.css";
-import background from "./../../assets/images/Biertan.jpg";
+import './Home.css';
+import background from './../../assets/images/Biertan.jpg';
 
 function Home() {
   return (
